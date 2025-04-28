c83077cd945da9504b5a30e72d254b282559d2d2
afiyataibani <afiyataibani@gmail.com>
2025-04-28 18:30:28 +0530

    spotify

---
+++ SpotifyAdmin/postcss.config.cjs
@@ -0,0 +1,7 @@
+// postcss.config.cjs
+module.exports = {
+  plugins: {
+    tailwindcss: {},  // Ensure this is configured here
+    autoprefixer: {},
+  },
+};
---
+++ SpotifyAdmin/src/pages/ListAlbum.jsx
@@ -0,0 +1,88 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { url } from "../App";
+import { toast } from "react-toastify";
+import { useState } from "react";
+
+const ListAlbum = () => {
+  const [data, setData] = useState([]);
+
+  const fetchAlbums = async () => {
+    try {
+      const response = await axios.get(`${url}/api/album/list`);
+
+      if (response.data.success) {
+        setData(response.data.albums);
+      }
+    } catch (error) {
+      toast.error("Error ocurred");
+    }
+  };
+
+  const removeAlbum = async (id) => {
+    try {
+      const response = await axios.post(`${url}/api/album/remove`, { id });
+
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchAlbums();
+      }
+    } catch (error) {
+      toast.error("Error occured");
+    }
+  };
+
+  useEffect(() => {
+    fetchAlbums();
+  }, []);
+
+  return (
+    <div>
+      <p>All Albums List</p>
+      <br />
+      <div>
+        <div className="sm:grid hidden grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5 bg-gray-100">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Description</b>
+          <b>Album Colour</b>
+          <b>Action</b>
+        </div>
+        {data.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="grid grid-cols-[1fr_1fr_1fr] sm:grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5"
+            >
+              <img className="w-12" src={item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.desc}</p>
+              <input type="color" value={item.bgColor} />
+              <p
+                onClick={() => removeAlbum(item._id)}
+                className="cursor-pointer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ListAlbum;
