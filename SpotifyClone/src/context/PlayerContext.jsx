5cd48978f608c63eac737c45c765e23b916bcac2
afiyataibani <142826346+afiyataibani@users.noreply.github.com>
2025-04-28 18:48:07 +0530

    Update PlayerContext.jsx

--- SpotifyClone/src/context/PlayerContext.jsx
+++ SpotifyClone/src/context/PlayerContext.jsx
@@ -8,7 +8,7 @@ const PlayerContextProvider = (props) => {
     const audioRef = useRef();
     const seekBg = useRef();
     const seekBar = useRef();
-    const url = 'http://localhost:4000'
+    const url = 'https://spotifyfullstack-backend.onrender.com'
 
     const [songsData, setSongsData] = useState([]);
     const [albumsData,setAlbumData] = useState([]);
