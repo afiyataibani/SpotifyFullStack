5f7df6cb8b1fc9059163a50d2408908a16f91beb
afiyataibani <142826346+afiyataibani@users.noreply.github.com>
2025-04-28 18:50:44 +0530

    Update App.jsx

--- SpotifyAdmin/src/App.jsx
+++ SpotifyAdmin/src/App.jsx
@@ -9,7 +9,7 @@ import ListAlbum from './pages/listAlbum';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
-export const url = 'http://localhost:4000'
+export const url = 'https://spotifyfullstack-backend.onrender.com'
 
 const App = () => {
   return (
